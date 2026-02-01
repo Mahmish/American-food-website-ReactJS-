aeb688e51706f1b75b125a7f0a05f38d5401644a
Mahmish Munawar <mahmishmunawar877@gmail.com>
2026-02-01 19:24:48 +0500

    app.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename="American-food-website-ReactJS-">
         <Header/>
         <Routes>
           <Route path='/' element={<Home/>}></Route>
